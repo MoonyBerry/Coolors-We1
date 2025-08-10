c465b7e40d678621db94fed8dce2915e95b687ad
Martina Tarantino <martinatarantino02@gmail.com>
2025-08-10 19:55:14 +0200

    Fixati alcuni errori

--- components/navbar_principale/navbar_principale.js
+++ components/navbar_principale/navbar_principale.js
@@ -6,10 +6,11 @@ const $btnMobile = document.querySelector(".fa-bars");
 $btnMobile.addEventListener("click", () => {
   $mobileNav.classList.add("navOpen");
   $navOverlay.classList.add("navOpen");
-  navOpen ? ($body.style.overflow = "hidden") : ($body.style.overflow = "auto");
+  $body.style.overflow = "hidden";
 });
 
 $navOverlay.addEventListener("click", () => {
   $mobileNav.classList.remove("navOpen");
   $navOverlay.classList.remove("navOpen");
+  $body.style.overflow = "auto";
 });
