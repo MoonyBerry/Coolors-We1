1c71d6b878a4fc9da33fb9429af7bcaa36a62d8d
MoonyBerry <martinatarantino02@gmail.com>
2025-08-14 15:26:23 +0200

    Revert "Montaggio/seconda pagina"

--- assets/js/generatore.js
+++
@@ -1,6 +0,0 @@
-/*
-    File che contiene tutte le importazioni dei file js tramite "import per la seconda pagina"
-*/
-
-import "../../components/navbar_principale/navbar_principale.js";
-import "../../components/palette_colori/palette_colori.js";
--- assets/js/home.js
+++
@@ -1,8 +0,0 @@
-/*
-    File che contiene tutte le importazioni dei file js tramite "import" per la prima pagina
-*/
-
-import "../../components/cards/cards.js";
-import "../../components/footer/footer.js";
-import "../../components/home-main/main.js";
-import "../../components/navbar_principale/navbar_principale.js";
