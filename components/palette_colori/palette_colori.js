b3dd5fc6a75f2b1cabbace567bedabcd281815e7
Martina Tarantino <martinatarantino02@gmail.com>
2025-08-19 10:08:31 +0200

    Aggiunta funzionalità copia hexcode

--- components/palette_colori/palette_colori.js
+++ components/palette_colori/palette_colori.js
@@ -19,3 +19,32 @@ container.addEventListener("click", (event) => {
     btn.closest(".optionContainer").classList.remove("lockVisible");
   }
 });
+
+//COPY HEXCODE BUTTON
+const $copyButton = document.querySelectorAll(".icon.clone");
+const $copyMessage = document.querySelector("div.success-copy-hexcode");
+
+$copyButton.forEach((copy) => {
+  copy.addEventListener("click", () => {
+    const container = copy.closest(".colorContainer");
+    const hexCode = "#" + container.querySelector(".description h1").innerHTML;
+    //TO COPY CODE TO CLIPBOARD
+    navigator.clipboard
+      .writeText(hexCode)
+      .then(() => {
+        $copyMessage.innerHTML = `<i class="fa-solid fa-circle-check" style="color: #ffffff"></i> Colore copiato negli appunti!`;
+        $copyMessage.classList.add("showMessage");
+        //REMOVING SHOWMESSAGE AFTER 3 SECONDS
+        setTimeout(() => {
+          $copyMessage.classList.remove("showMessage");
+        }, 3000);
+      })
+      .catch(() => {
+        $copyMessage.innerHTML = "Errore, codice inesistene";
+        $copyMessage.classList.add("showMessage");
+        setTimeout(() => {
+          $copyMessage.classList.remove("showMessage");
+        }, 3000);
+      });
+  });
+});
