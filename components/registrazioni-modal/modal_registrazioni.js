19081531527e4dcae874654dc348ca64073df3b0
Andrea Cesare Trapani <161830645+AndreaTrapani97@users.noreply.github.com>
2025-09-15 17:52:46 +0200

    intallata tutta la modal inscrizioni

--- components/registrazioni-modal/modal_registrazioni.js
+++ components/registrazioni-modal/modal_registrazioni.js
@@ -1,28 +1,33 @@
-const btnXRegistration = document.querySelector(".btnX");
+window.addEventListener("DOMContentLoaded", function () {
+  const btnXRegistration = document.querySelector(".btnX");
+  const overlayRegistration = document.querySelector(
+    ".overlay-modal-inscrizione"
+  );
+  const containerRegistration = document.querySelector(
+    ".container-modal-registrazione"
+  );
+  const btnRegister = document.querySelector(".btn-register");
+  const btnLogin = document.querySelector(".log-in");
+  const btnLoginMobile = document.querySelector(".btn-log-in-mobile");
+  const btnRegisterMobile = document.querySelector(".btn-register-mobile");
+  const mobileNav = this.document.querySelector(".mobile-nav");
+  const overlayNav = this.document.querySelector(".overlay");
 
-const overlayRegistration = document.querySelector(".overlay");
-const containerRegistration = document.querySelector(
-  ".container-modal-registrazione"
-);
+  if (btnRegister) btnRegister.addEventListener("click", apriModal);
+  if (btnLogin) btnLogin.addEventListener("click", apriModal);
+  if (btnRegisterMobile) btnRegisterMobile.addEventListener("click", apriModal);
+  if (btnLoginMobile) btnLoginMobile.addEventListener("click", apriModal);
+  if (btnXRegistration) btnXRegistration.addEventListener("click", chiudiModal);
 
-const btnRegister = document.querySelector(".btn-register");
-const btnLogin = document.querySelector(".log-in");
-const btnLoginMobile = document.querySelector(".btn-log-in-mobile");
-const btnRegisterMobile = document.querySelector(".btn-register-mobile");
+  function apriModal() {
+    overlayRegistration.classList.add("is-visible");
+    containerRegistration.classList.add("is-visible");
+    mobileNav.classList.remove("navOpen");
+  }
 
-btnRegister.addEventListener("click", apriModal);
-btnLogin.addEventListener("click", apriModal);
-btnRegisterMobile.addEventListener("click", apriModal);
-btnLoginMobile.addEventListener("click", apriModal);
-
-btnXRegistration.addEventListener("click", chiudiModal);
-
-function apriModal() {
-  overlayRegistration.classList.add("is-visible");
-  containerRegistration.classList.add("is-visible");
-}
-
-function chiudiModal() {
-  overlayRegistration.classList.remove("is-visible");
-  containerRegistration.classList.remove("is-visible");
-}
+  function chiudiModal() {
+    overlayRegistration.classList.remove("is-visible");
+    containerRegistration.classList.remove("is-visible");
+    overlayNav.classList.remove("navOpen");
+  }
+});
